4c89c55fff7d1799fb68bc3457ef5e00d5412672
huylebron <doquochuy27102003@gmail.com>
2026-06-22 11:05:14 +0700

    java backend devops

---
+++ src/main/java/ra/java_backend_devops/JavaBackendDevopsApplication.java
@@ -0,0 +1,13 @@
+package ra.java_backend_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendDevopsApplication.class, args);
+    }
+
+}
